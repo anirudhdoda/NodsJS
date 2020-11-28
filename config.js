c023a799303110c71c90d46c94ab7f75128f40cc
anirudhdoda <anirudh1510.cse18@chitkara.edu.in>
2020-11-29 01:35:47 +0530

    oauth2_facebook passport'

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'423496142018144',
+        clientSecret:'d4c37728d782d06363e0ff825dd7aa2b'
+    }
 }
